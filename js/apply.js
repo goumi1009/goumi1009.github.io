bdc8c5a243674cbbf201e43349444a869ed41f53
goumi <goumi1009@gmail.com>
2020-12-29 09:51:34 +0900

    no message

--- js/apply.js
+++ js/apply.js
@@ -4,6 +4,11 @@ $(document).ready(function () {
     $(btn).on('click', function () {
       $(".tab-con").removeClass("on");
       $(".tab-con").eq(idx).addClass("on")
+
+      var price = $(this).data('price');
+      console.log(price)
+
+      $('.js-price').text(price)
     })
   })
 })
