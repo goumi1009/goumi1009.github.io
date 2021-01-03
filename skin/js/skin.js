336e5503e2ac797767c26915b05bebe703984ab6
goumi <goumi1009@gmail.com>
2021-01-04 00:33:31 +0900

    no message

--- skin/js/skin.js
+++ skin/js/skin.js
@@ -1,20 +1,17 @@
 $(document).ready(function () {
   $('.btn-m-toggle').on('click', function () {
-    $('.header').toggleClass('active');
-    $('body').toggleClass('no-scroll');
+    menuToggle()
   });
   ie_fixed();
 });
 
 $(window).scroll(function() {
   var st = $(window).scrollTop();
-  if(st >= 100 && !$('body').hasClass('main')) {
-    $('body').addClass('fix-tit');
-  } else {
-    $('body').removeClass('fix-tit');
-  }
+  var fixH = $('.info-head').height();
+  headerChange (st);
 });
 
+// 이미지 가로세로 비율체크
 function setDirection(element) {
   if (element.naturalWidth / element.naturalHeight / element.parentNode.offsetWidth * element.parentNode.offsetHeight > 1) {
     element.classList.add('horizontal');
@@ -23,8 +20,7 @@ function setDirection(element) {
   }
 }
 
-
-/* ie background-fixed 튐 현상 해결을 위한 스크립트 */
+// ie fixed 튐 현상 해결을 위한 스크립트
 function ie_fixed(){
     if(navigator.userAgent.match(/Trident\/7\./)) { // if IE
         $('body').on("mousewheel", function () {
@@ -37,4 +33,21 @@ function ie_fixed(){
             window.scrollTo(0, currentScrollPosition - wheelDelta);
         });
     }
+}
+
+// .info-head style변경
+function headerChange(scrollT) {
+  if(scrollT >= 100 && !$('body').hasClass('main')) {
+    $('body').addClass('fix-tit');
+  } else {
+    $('body').removeClass('fix-tit');
+  }
+
+  
+}
+
+// menu 열고 닫고
+function menuToggle() {
+  $('.header').toggleClass('active');
+  $('body').toggleClass('no-scroll');
 }
